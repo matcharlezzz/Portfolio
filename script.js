bebcc06d95453cd5d64d6f2e6728e3590f41ed2e
magniefique <bandalacharlesandre@gmail.com>
2024-04-13 02:17:54 +0800

    Updated Code

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ function random_digit(){
     return digit;
 }
 
-function rain(){
-    const rain_cont = document.getElementById("rain_cont");
-
+function rain(rain_cont, font_size, anim, death_rate){
     let digit = document.createElement('p');
     let left = Math.floor(Math.random() * 100);
 
@@ -15,15 +13,23 @@ function rain(){
     rain_cont.appendChild(digit);
     digit.innerText = random_digit();
     digit.style.left = left + '%';
+    digit.style.fontSize = font_size + 'pt';
+    digit.style.animation = anim;
 
     setTimeout(function(){
         rain_cont.removeChild(digit);
-    }, 5000);
+    }, death_rate);
 }
 
+const rain_cont = document.querySelectorAll("#rain_cont");
+console.log(rain_cont);
 setInterval(function(){
-    rain();
-}, 100)
+    rain(rain_cont[0], 52, "rain linear 5s", 5000);
+}, 100);
+
+setInterval(function(){
+    rain(rain_cont[1], 32, "rain linear 15s", 15000);
+}, 200);
 
 /* Dynamic Navigation Button */
 const home = document.querySelectorAll("#home");
@@ -33,7 +39,7 @@ const header_ob = new IntersectionObserver((entries) => {
         nav_butt.classList.toggle("nav_active", !entry.isIntersecting)
     });
 }, {
-    threshold: 0.7
+    threshold: 0.5
 });
 
 home.forEach((el) => header_ob.observe(el));
@@ -68,18 +74,25 @@ window.onload = animate_desc;
 
 /* About Lights */
 const about_page = document.querySelectorAll("#about");
+const about_bg = document.querySelectorAll(".bg_inact");
 const desc_img = document.getElementById("desc_img");
 const desc_header = document.getElementById("desc_header");
 const desc_subheader = document.getElementById("desc_subheader");
 const desc_info = document.querySelectorAll("#desc_info");
 const about_ob = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        desc_img.classList.toggle('desc_img_act', entry.isIntersecting);
-        desc_header.classList.toggle('desc_active_h1', entry.isIntersecting);
-        desc_subheader.classList.toggle('desc_active_h2', entry.isIntersecting);
+        function light_bg (el){
+            el.classList.toggle('bg_active', entry.isIntersecting);
+        }
+        
         function light_p (el){
             el.classList.toggle('desc_active_p', entry.isIntersecting);
         }
+        
+        about_bg.forEach((el) => light_bg(el));
+        desc_img.classList.toggle('desc_img_act', entry.isIntersecting);
+        desc_header.classList.toggle('desc_active_h1', entry.isIntersecting);
+        desc_subheader.classList.toggle('desc_active_h2', entry.isIntersecting);
         desc_info.forEach((el) => light_p(el));
     });
 }, {
@@ -87,3 +100,14 @@ const about_ob = new IntersectionObserver((entries) => {
 });
 
 about_page.forEach((el) => about_ob.observe(el));
+
+const about_btn = document.getElementById("btn_about");
+
+const about_edge_ob = new IntersectionObserver ((entries) => {
+    entries.forEach((entry) => {
+        about_btn.classList.toggle("btn_active", entry.isIntersecting);
+    });
+}, {
+    threshold: 0.95
+});
+about_page.forEach((el) => about_edge_ob.observe(el));
